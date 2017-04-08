368323cf27dc04f2aba29d962821b6bd75e67908
hy-chao <hyccauc2015@163.com>
2017-04-08 11:41:03 +0800

    第二章完成

--- EssentialC++2/2.cpp
+++ EssentialC++2/2.cpp
@@ -5,6 +5,7 @@
 */
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 /*
@@ -96,4 +97,174 @@ int main()
 	display(a);
 	return 0;
 }
+*/
+
+/*
+//Á·Ï°2.4
+const vector<int>* PentagonalInit(int size)
+{
+	static vector<int> a;
+	if (a.size() < size)
+	{
+		for (int i = a.size() + 1; i < size + 1; i++)
+		{
+			a.push_back(i*(3 * i - 1) / 2);
+		}
+	}
+	
+	return &a;
+}
+int recall_single_elem(const vector<int>* a, int n)
+{
+	if (n >= a->size())
+	{
+		cerr << "input error!" << endl;
+		return -1;
+	}
+	return (*a)[n];
+}
+
+int main()
+{
+	const vector<int> *a = PentagonalInit(10);
+	cout << recall_single_elem(a, 3);
+	return 0;
+}
+*/
+
+/*
+//Á·Ï°2.5
+void max(int a, int b)
+{
+	if (a < b)
+	{
+		cout << b << endl;
+	}
+	else
+	{
+		cout <<  a << endl;
+	}
+}
+
+void max(float a, float b)
+{
+	if (a < b)
+	{
+		cout <<  b << endl;
+	}
+	else
+	{
+		cout <<  a << endl;
+	}
+}
+
+void max(char* a, char* b)
+{
+	if (strcmp(a, b) > 0)
+	{
+		cout <<  a << endl;
+	}
+	else
+	{
+		cout <<  b << endl;
+	}
+}
+
+void max(vector<int> a)
+{
+	int re = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (re < a[i])
+		{
+			re = a[i];
+		}
+	}
+	cout << re << endl;
+}
+void max(vector<float> a)
+{
+	float re = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (re < a[i])
+		{
+			re = a[i];
+		}
+	}
+	cout << re << endl;
+}
+void max(vector<char*> a)
+{
+	char* re = NULL;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (re < a[i])
+		{
+			re = a[i];
+		}
+	}
+	cout << re << endl;
+}
+
+int main()
+{
+	max(1, 3);
+	max(1.2f, 1.5f);
+	max("adda","adfas");
+	vector<int> a = {2, 4};
+	vector<float> b = {2.3f, 2.5f};
+	vector<char*> c = {"sdad", "sdadsd"};
+	max(a);
+	max(b);
+	max(c);
+	return 0;
+}
+*/
+/*
+//Á·Ï°2.6
+template <typename elemType >
+void max(elemType a, elemType b)
+{
+	if (a < b)
+	{
+		cout << b << endl;
+	}
+	else
+	{
+		cout << a << endl;
+	}
+}
+template <typename elemType >
+void max(vector<elemType> a)
+{
+	elemType re = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (re < a[i])
+		{
+			re = a[i];
+		}
+	}
+	cout << re << endl;
+}
+int main()
+{
+	max(1, 3);
+	max(1.2f, 1.5f);
+	max("adda", "adfas");
+	vector<int> a = { 2, 4 };
+	vector<float> b = { 2.3f, 2.5f };
+	vector<char*> c = { "sdad", "sdadsd" };
+	max(a);
+	max(b);
+	max(c);
+	return 0;
+}
+*/
+
+/*
+C++Á·Ï°Ìâ£ºEssential C++ µÚÈýÕÂ
+author£ºhyc
+date£º2017.4.8
 */
