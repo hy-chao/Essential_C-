2ab776abbed842326dcdbac9954b8b7040cba73f
hy-chao <hyccauc2015@163.com>
2017-04-26 21:54:27 +0800

    6.1

--- EssentialC++2/6.cpp
+++ EssentialC++2/6.cpp
@@ -8,4 +8,35 @@
 #include<vector>
 
 using namespace std;
+//Á·Ï°6.1
+/*
+template <typename elemType> 
+class example
+{
+public:
+	example(elemType min, elemType max);
+	example(const elemType *array, int size);
+	elemType& operator[](int index);
+	bool operator==(const example&)const;
+	bool insert(const elemType*, int);
+	bool insert(elemType);
+	elemType min() const
+	{
+		return _min;
+	}
+	elemType max() const
+	{
+		return _max;
+	}
+	void min(const elemType&);
+	void max(const elemType&);
+	int count(const elemType &value) const;
+private:
+	int size;
+	elemType *parray;
+	elemType _min;
+	elemType_max;
+};
+*/
+//Á·Ï°6.2
 
