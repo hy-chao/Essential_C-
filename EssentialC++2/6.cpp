f2917ac4468dd17508eaa6fc2c9b75fbb1eaf4e1
hy-chao <hyccauc2015@163.com>
2017-04-28 21:21:31 +0800

    第6章完成

--- EssentialC++2/6.cpp
+++ EssentialC++2/6.cpp
@@ -39,4 +39,102 @@ private:
 };
 */
 //Á·Ï°6.2
+/*
+template <typename elemType>
+class Matrix
+{
+	friend Matrix<elemType> operator+(const Matrix<elemType>&, const Matrix<elemType>&);
+	friend Matrix<elemType> operator*(const Matrix<elemType>&, const Matrix<elemType>&);
+public:
+	Matrix(int rows, int columns);
+	Matrix(const Matrix);
+	~Matrix();
+	Matrix& operator=(const Matrix&);
+	void operator+=(const Matrix&);
+	elemType& operator()(int row, int column)
+	{
+		return _matrix[row * cols() + column];
+	}
+	const elemType& operator()(int row, int column) const
+	{
+		return _matrix[row * cols() + column];
+	}
+	int rows() const
+	{
+		return _rows;
+	}
+	int cols() const
+	{
+		return _cols;
+	}
+	bool same_size(const Matrix& m) const
+	{
+		return rows() = m.rows() && cols() == m.cols();
+	}
+	bool comfortable(const Matrix &m) const
+	{
+		return cols() == m.rows();
+	}
+	ostream& print(ostream&) const;
+protected:
+	int _rows;
+	int _cols;
+	elemType *_matrix;
+};
+template <typename elemType>
+inline ostream& operator<<(ostream& os, const Matrix<elemType> &m)
+{
+	return m.print(os);
+}
 
+template <typename elemType>
+Matrix<elemType> operator+(const Matrix<elemType> &m1, const Matrix<elemType> &m2)
+{
+	Matrix<elemType> result(m1);
+	result += m2;
+	return result;
+}
+
+template <typename elemType>
+Matrix<elemType> operator*(const Matrix<elemType> &m1, const Matrix<elemType> &m2)
+{
+	Matrix<elemType> result(m1.rows(), m2.cols());
+	for (int ix = 0; ix < m1.rows(); ix++)
+	{
+		for (int jx = 0; jx < m1.cols(); jx++)
+		{
+			result(ix, jx) = 0;
+		}
+	}
+	for (int kx = 0; kx < m1.cols(); kx++)
+	{
+		result(ix, jx) += m1(ix, kx) + m2(kx, jx);
+	}
+	return result;
+}
+
+template <typename elemType>
+void Matrix<elemType>::operator+=(const Matrix &m)
+{
+	int matrix_size = cols() * rows();
+	for (int ix = 0; ix < matrix_size(); ++ix)
+	{
+		(*(_matrix + ix)) += (*(m._matrix + ix));
+	}
+}
+
+template <typename elemType>
+void Matrix<elemType>::print(ostream &os) const
+{
+	int col = cols();
+	int matrix_size = col * rows();
+	for (int ix = 0; ix < matrix_size; ++i)
+	{
+		if (ix % col == 0)
+			os << endl;
+		os << (*(_matrix + ix)) << ' ';
+	}
+	os << endl;
+	return os;
+}
+*/
